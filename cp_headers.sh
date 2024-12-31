a9a4871637706e0597051323a5d5be4e881f26f3
Pavel Dubrova <pashadubrova@gmail.com>
2024-12-31 15:27:34 +0200

    cp_headers: Sort headers alphabetically
    This is just a cosmetic change that sorts the headers
    alphabetically and removes duplicates.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- cp_headers.sh
+++ cp_headers.sh
@@ -9,56 +9,55 @@ LINUX_HEADERS="\
     linux/hdmi.h"
 
 UAPI_HEADERS="\
-    scsi/ufs/ufs.h\
-    scsi/ufs/ioctl.h\
-    sound/compress_params.h\
-    sound/compress_offload.h\
-    sound/asound.h\
-    linux/ion.h\
-    linux/msm_ion.h\
-    linux/msm_ion_ids.h\
-    linux/v4l2-mediabus.h\
+    asm-generic/ioctls.h\
+    drm/drm.h\
+    drm/drm_fourcc.h\
+    drm/drm_mode.h\
+    drm/msm_drm.h\
     linux/esoc_ctrl.h\
-    linux/msm_rmnet.h\
-    linux/msm_ipa.h\
-    linux/videodev2.h\
     linux/fb.h\
     linux/fuse.h\
+    linux/ion.h\
     linux/ipa_qmi_service_v01.h\
-    linux/netlink.h\
-    linux/msm_kgsl.h\
+    linux/mmc/ioctl.h\
     linux/msm_dsps.h\
+    linux/msm_ion.h\
+    linux/msm_ion_ids.h\
+    linux/msm_ipa.h\
+    linux/msm_kgsl.h\
+    linux/msm_rmnet.h\
+    linux/netlink.h\
+    linux/qseecom.h\
     linux/rmnet_data.h\
     linux/rmnet_ipa_fd_ioctl.h\
     linux/socket.h\
-    linux/qseecom.h\
     linux/spcom.h\
     linux/smcinvoke.h\
     linux/v4l2-common.h\
     linux/v4l2-controls.h\
     linux/v4l2-mediabus.h\
-    linux/mmc/ioctl.h\
-    drm/msm_drm.h\
-    drm/drm_fourcc.h\
-    drm/drm.h\
-    drm/drm_mode.h\
-    asm-generic/ioctls.h"
+    linux/videodev2.h\
+    scsi/ufs/ioctl.h\
+    scsi/ufs/ufs.h\
+    sound/asound.h\
+    sound/compress_offload.h\
+    sound/compress_params.h"
 
 TECHPACK_AUDIO_UAPI_HEADERS="\
-    sound/audio_compressed_formats.h\
-    sound/audio_effects.h\
-    sound/lsm_params.h\
-    sound/devdep_params.h\
-    sound/msmcal-hwdep.h\
-    sound/voice_params.h\
-    sound/wcd-dsp-glink.h\
-    linux/msm_audio_calibration.h\
-    linux/msm_audio_wmapro.h\
+    linux/msm_audio.h\
+    linux/msm_audio_aac.h\
     linux/msm_audio_amrwb.h\
     linux/msm_audio_amrwbplus.h\
+    linux/msm_audio_calibration.h\
     linux/msm_audio_wma.h\
-    linux/msm_audio_aac.h\
-    linux/msm_audio.h"
+    linux/msm_audio_wmapro.h\
+    sound/audio_compressed_formats.h\
+    sound/audio_effects.h\
+    sound/devdep_params.h\
+    sound/lsm_params.h\
+    sound/msmcal-hwdep.h\
+    sound/voice_params.h\
+    sound/wcd-dsp-glink.h"
 
 TECHPACK_AUDIO_PACK_UAPI_HEADERS="\
     wcd9320_registers.h\
@@ -72,6 +71,7 @@ TECHPACK_CAMERA_UAPI_HEADERS="\
     camera/media/cam_icp.h\
     camera/media/cam_isp.h\
     camera/media/cam_isp_ife.h\
+    camera/media/cam_isp_sfe.h\
     camera/media/cam_isp_tfe.h\
     camera/media/cam_isp_vfe.h\
     camera/media/cam_jpeg.h\
@@ -80,34 +80,21 @@ TECHPACK_CAMERA_UAPI_HEADERS="\
     camera/media/cam_req_mgr.h\
     camera/media/cam_sensor.h\
     camera/media/cam_sync.h\
-    camera/media/cam_tfe.h\
-    camera/media/cam_defs.h\
-    camera/media/cam_sensor.h\
-    camera/media/cam_isp.h\
-    camera/media/cam_isp_ife.h\
-    camera/media/cam_isp_sfe.h\
-    camera/media/cam_isp_vfe.h\
-    camera/media/cam_icp.h\
-    camera/media/cam_cpas.h\
-    camera/media/cam_fd.h\
-    camera/media/cam_jpeg.h\
-    camera/media/cam_lrme.h\
-    camera/media/cam_req_mgr.h\
-    camera/media/cam_sync.h"
+    camera/media/cam_tfe.h"
 
 TECHPACK_DISPLAY_UAPI_HEADERS="\
-    display/media/msm_sde_rotator.h\
-    display/media/mmm_color_fmt.h\
     display/drm/msm_drm_pp.h\
-    display/drm/sde_drm.h"
+    display/drm/sde_drm.h\
+    display/media/mmm_color_fmt.h\
+    display/media/msm_sde_rotator.h"
 
 TECHPACK_VIDEO_UAPI_HEADERS="\
     msm_media_info.h"
 
 HEADER_OVERRIDES="\
+    display/media/mmm_color_fmt.h\
     linux/socket.h\
-    media/msm_media_info.h\
-    display/media/mmm_color_fmt.h"
+    media/msm_media_info.h"
 
 cd ../../../..
 
